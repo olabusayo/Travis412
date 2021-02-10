cc4e5a5ca210c95054854e92de1691e9f68847d6
olabusayo <8975598+olabusayo@users.noreply.github.com>
2021-02-09 22:48:32 -0500

    TDD: Test for multiply and divide

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -17,4 +17,16 @@ public class SimpleCalculatorTest {
 		SimpleCalculator calc = new SimpleCalculator();
 		assertEquals(calc.sub(5, 3), 2);
 	}
+
+	@Test
+	public void testMultiply() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.mult(5, 2), 10);
+	}
+
+	@Test
+	public void testDivide() {
+		SimpleCalculator calc = new SimpleCalculator();
+		assertEquals(calc.div(9, 3), 3);
+	}
 }
