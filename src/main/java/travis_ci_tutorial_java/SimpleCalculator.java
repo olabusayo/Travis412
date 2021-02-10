49ba0a58b20f259503d0ac19601eb0d9b63e4c7a
olabusayo <8975598+olabusayo@users.noreply.github.com>
2021-02-10 00:12:03 -0500

    TDD: add implementation for mult and divide; also fix readme and update gitignore

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -5,4 +5,6 @@ public class SimpleCalculator {
 		return a + b;
 	}
 	public int sub(int a, int b) { return a - b; }
+	public int mult(int a, int b) { return a * b; }
+	public int div(int a, int b) { return a / b; }
 }
